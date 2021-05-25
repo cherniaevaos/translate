8d454d4c3811e2585f8b3b6732aa457c77d446a6
cherniaevaos <71915509+cherniaevaos@users.noreply.github.com>
2021-05-25 23:47:35 +1000

    Create funct.h

---
+++ funct.h
@@ -0,0 +1,10 @@
+#ifndef UNTITLED5_FUNCTION_H
+#define UNTITLED5_FUNCTION_H
+
+char* str = new char[500];
+const char* ang = "ABVGDEIoGZIIKLMNOPRSTYFHCChShShEUYaabvgdeiogziiklmnoprstyfhcchshshyieuyai'";
+const char* rus = "АБВГДЕЁ*ЖЗИЙКЛМНОПРСТУФХЦЧ*Ш*Щ*ЭЮЯ*абвгдеё*жзийклмнопрстуфхцч*ш*щ*ы*эюя*ьъ.";
+
+void trans(const char *str, const char *ang, const char *rus);
+
+#endif //UNTITLED5_FUNCTION_H
